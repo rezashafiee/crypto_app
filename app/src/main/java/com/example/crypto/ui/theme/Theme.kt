bab1e9dc075c212e21239788cb9bb672be736425
reza <mrshafiee@hotmail.com>
2025-07-15 20:11:41 +0200

    change theme name

--- app/src/main/java/com/example/crypto/ui/theme/Theme.kt
+++ app/src/main/java/com/example/crypto/ui/theme/Theme.kt
@@ -253,7 +253,7 @@ val unspecified_scheme = ColorFamily(
 )
 
 @Composable
-fun AppTheme(
+fun CryptoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
