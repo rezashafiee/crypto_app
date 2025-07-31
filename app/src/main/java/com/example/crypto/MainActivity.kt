a1511dcf532e5a3961aa8959b7f3451404137d20
Mohammadreza Shafiee <mrshafiee@hotmail.com>
2025-07-31 23:13:11 +0200

    update navigation

--- app/src/main/java/com/example/crypto/MainActivity.kt
+++ app/src/main/java/com/example/crypto/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.material3.adaptive.layout.AnimatedPane
 import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
 import androidx.compose.material3.adaptive.navigation.NavigableListDetailPaneScaffold
 import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
@@ -18,7 +19,9 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.crypto.core.presentation.theme.CryptoTheme
 import com.example.crypto.feature.crypto.presentation.coin_detail.CoinDetailScreen
 import com.example.crypto.feature.crypto.presentation.coin_list.CoinListScreen
+import com.example.crypto.feature.crypto.presentation.coin_list.CoinListUiState
 import com.example.crypto.feature.crypto.presentation.coin_list.CoinListViewModel
+import com.example.crypto.feature.crypto.presentation.models.CoinUi
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -33,28 +36,35 @@ class MainActivity : ComponentActivity() {
                     val viewModel by viewModel<CoinListViewModel>()
                     val state = viewModel.state.collectAsStateWithLifecycle()
 
-                    val listDetailNavigator = rememberListDetailPaneScaffoldNavigator<Nothing>()
+                    val listDetailNavigator = rememberListDetailPaneScaffoldNavigator<CoinUi>()
                     val scope = rememberCoroutineScope()
 
                     NavigableListDetailPaneScaffold(
                         navigator = listDetailNavigator,
                         listPane = {
-                            CoinListScreen(
-                                state.value,
-                                onItemClick = {
-                                    scope.launch {
-                                        listDetailNavigator.navigateTo(
-                                            ListDetailPaneScaffoldRole.Detail
-                                        )
+                            AnimatedPane {
+                                CoinListScreen(
+                                    state.value,
+                                    onItemClick = {
+                                        scope.launch {
+                                            listDetailNavigator.navigateTo(
+                                                ListDetailPaneScaffoldRole.Detail,
+                                                state.value.selectedCoin
+                                            )
+                                        }
                                     }
-                                }
-                            )
+                                )
+                            }
                         },
                         detailPane = {
-                            CoinDetailScreen(
-                                state = state.value,
-                                windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
-                            )
+                            AnimatedPane {
+                                listDetailNavigator.currentDestination?.contentKey?.let {
+                                    CoinDetailScreen(
+                                        state = state.value,
+                                        windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+                                    )
+                                }
+                            }
                         }
                     )
 
