00e5936d5632ead59e1d7132cfa05053ce59f50a
reza <mrshafiee@hotmail.com>
2025-09-02 23:49:56 +0200

    update koin constructor dsl

--- app/src/main/java/com/example/crypto/di/AppModule.kt
+++ app/src/main/java/com/example/crypto/di/AppModule.kt
@@ -8,38 +8,27 @@ import com.example.crypto.feature.crypto.data.CoinListLocalDataSourceImp
 import com.example.crypto.feature.crypto.data.CoinListRemoteDataSourceImp
 import com.example.crypto.feature.crypto.data.CoinListRepositoryImp
 import com.example.crypto.feature.crypto.domain.CoinListLocalDataSource
+import com.example.crypto.feature.crypto.domain.CoinListRemoteDataSource
 import com.example.crypto.feature.crypto.domain.CoinListRepository
 import com.example.crypto.feature.crypto.presentation.coin_list.CoinListViewModel
 import io.ktor.client.engine.cio.CIO
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
     single { HttpClientFactory.create(CIO.create()) }
-    single<com.example.crypto.feature.crypto.domain.CoinListRemoteDataSource> {
-        CoinListRemoteDataSourceImp(
-            httpClient = get()
-        )
-    }
-    single<CoinListLocalDataSource> {
-        CoinListLocalDataSourceImp(
-            coinDao = get()
-        )
-    }
+    single { MutableSharedFlow<RemoteSyncResult>() }
+    single { get<MutableSharedFlow<RemoteSyncResult>>().asSharedFlow() }
 
-    single { MutableSharedFlow<RemoteSyncResult>() } // For injection into the worker
-    single { get<MutableSharedFlow<RemoteSyncResult>>().asSharedFlow() } // For injection into the repository (immutable)
+    singleOf(::CoinListRemoteDataSourceImp) { bind<CoinListRemoteDataSource>() }
+    singleOf(::CoinListLocalDataSourceImp) { bind<CoinListLocalDataSource>() }
+    singleOf(::CoinListRepositoryImp) { bind<CoinListRepository>() }
 
-    single<CoinListRepository> {
-        CoinListRepositoryImp(
-            localDataSource = get(),
-            context = androidContext(),
-            remoteSyncEvents = get()
-        )
-    }
     single {
         Room.databaseBuilder(
             androidContext(),
