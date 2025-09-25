87dc779adbcff985ec7c0fe41b93572109f60a97
reza <mrshafiee@hotmail.com>
2025-09-25 23:15:56 +0200

    fix ktlint issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,22 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.ktlint)
+    alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        if (project.hasProperty("enableKtlintDebug")) {
+            debug.set(true)
+        } else {
+            debug.set(false)
+        }
+    }
 }
 
 tasks.register<Exec>("konsistCheck") {
